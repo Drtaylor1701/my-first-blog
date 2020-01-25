f189af9a6929c8c72304b54d511bbe4374d01be8
Gillian Weisgram <trekker35@gmail.com>
2020-01-24 17:05:25 -0800

    created models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
-
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+#model defined for blog post
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
